2e919844feba504732ac88f0cd28554576f7eb19
tweiss <tobias.weiss@bearingpoint.com>
2023-02-07 09:47:48 +0100

    dispatch event on activeElement

--- scripts/content.js
+++ scripts/content.js
@@ -1,25 +1,23 @@
 document.addEventListener("keydown", function (event) {
   if (event.key == "n" && event.ctrlKey == true) {
-    document.dispatchEvent(
-      new KeyboardEvent("keydown", {
-        bubbles: true,
-        cancelable: true,
-        key: "ArrowDown",
-        code: "ArrowDown",
-        keyCode: 40,
-      })
-    );
+    const keyDownEvent = new KeyboardEvent("keydown", {
+      bubbles: true,
+      cancelable: true,
+      key: "ArrowDown",
+      code: "ArrowDown",
+      keyCode: 40,
+    });
+    document.activeElement.dispatchEvent(keyDownEvent);
   }
 
   if (event.key == "p" && event.ctrlKey == true) {
-    document.dispatchEvent(
-        new KeyboardEvent("keydown", {
-          bubbles: true,
-          cancelable: true,
-          key: "ArrowUp",
-          code: "ArrowUp",
-          keyCode: 38,
-        })
-      );
+    const keyUpEvent = new KeyboardEvent("keydown", {
+      bubbles: true,
+      cancelable: true,
+      key: "ArrowUp",
+      code: "ArrowUp",
+      keyCode: 38,
+    });
+    document.activeElement.dispatchEvent(keyUpEvent);
   }
 });
